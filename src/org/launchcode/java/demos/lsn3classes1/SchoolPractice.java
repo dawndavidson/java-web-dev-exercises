d3b3e109fe3467d290184e4bfb12429088f027b5
dawndavidson <84029532+dawndavidson@users.noreply.github.com>
2021-08-03 10:52:52 -0500

    Exercise for chapter 5

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -6,7 +6,7 @@ public class SchoolPractice {
     public static void main(String[] args) {
         Student firstStudent = new Student("Dawn", 156525, 1, 4.0);
 
-        System.out.println(firstStudent.getStudentId());
+        System.out.println("The GPA for this student is " + firstStudent.getGpa() + "The Name of this student is " + firstStudent.getName() + firstStudent.getStudentId() + firstStudent.getNumberOfCredits());
         // Instantiate your Student class for part 2 here!
     }
 
