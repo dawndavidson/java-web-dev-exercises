a8db7fc4e4496d4c735ef3d479d8bc24265ddd47
dawndavidson <84029532+dawndavidson@users.noreply.github.com>
2021-08-27 17:55:55 -0500

    Exercise for chapter 5

--- src/org/launchcode/java/demos/lsn6inheritance/Cat.java
+++ src/org/launchcode/java/demos/lsn6inheritance/Cat.java
@@ -1,6 +1,7 @@
 package org.launchcode.java.demos.lsn6inheritance;
 
-public class Cat {
+public  class Cat
+{
 
     private boolean tired = false;
     private boolean hungry = false;
@@ -11,6 +12,10 @@ public class Cat {
 
     public Cat (double aWeight) {
         weight = aWeight;
+    }
+
+    public Cat () {
+        weight = 13;
     }
 
     /**** Getters and Setters ****/
